d80d4e1b8af994220b319dac6c92ae0f54c80b89
Ігнатюк Андрій Мирославович <ipz231_iam@student.ztu.edu.ua>
2026-05-11 18:34:47 +0300

    feat: add crud for users

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { type User } from './interfaces/user.interface';
+import { CreateUserDto } from './dto/create-user.dto';
+import { PatchUserDto } from './dto/patch-user.dto';
 
-@Controller()
+@Controller('users')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getUsers(): User[] {
+        return this.appService.getUsers()
+    }
+
+    @Get(':userId')
+    getUser(
+        @Param('userId') userId: string
+    ): User {
+        return this.appService.getUser(userId)
+    }
+
+    @Post()
+    createUser(
+        @Body() dto: CreateUserDto
+    ): User {
+        return this.appService.createUser(dto)
+    }
+
+    @Delete(':userId')
+    deleteUser(
+        @Param('userId') userId: string
+    ): User {
+        return this.appService.deleteUser(userId)
+    }
+
+    @Patch(':userId')
+    patchUser(
+        @Param('userId') userId: string,
+        @Body() dto: PatchUserDto
+    ): User {
+        return this.appService.patchUser(userId, dto)
+    }
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,88 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { type User } from './interfaces/user.interface';
+import { type CreateUserDto } from './dto/create-user.dto';
+import { type PatchUserDto } from './dto/patch-user.dto';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    private users = [
+        {
+            id: '0',
+            name: 'user0'
+        },
+        {
+            id: '1',
+            name: 'user1'
+        },
+        {
+            id: '2',
+            name: 'user2'
+        },
+        {
+            id: '3',
+            name: 'user3'
+        },
+        {
+            id: '4',
+            name: 'user4'
+        },
+        {
+            id: '5',
+            name: 'user5'
+        },
+    ]
+
+    getUsers(): User[] {
+        return this.users
+    }
+
+    getUser(userId: string): User {
+        const user = this.users.find((user) => user.id === userId)
+
+        if (!user) {
+            throw new NotFoundException('User is not found')
+        }
+
+        return user
+    }
+
+    createUser(dto: CreateUserDto): User {
+        const { name } = dto
+
+        // create user in db
+
+        return {
+            id: "10",
+            name: name
+        }
+    }
+
+    deleteUser(userId: string): User {
+        const user = this.users.find((user) => user.id === userId)
+
+        if (!user) {
+            throw new NotFoundException('User is not found')
+        }
+
+        // delete user in db
+
+        return user
+    }
+
+    patchUser(userId: string, dto: PatchUserDto): User {
+        const { name } = dto
+
+        const user = this.users.find((user) => user.id === userId)
+
+        if (!user) {
+            throw new NotFoundException('User is not found')
+        }
+
+        // patch user in db
+
+        return {
+            'id': user.id,
+            'name': name
+        }
+    }
 }
